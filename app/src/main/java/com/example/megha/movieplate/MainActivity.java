2330a14fe0e39641174f7ebb1da34e750e72f65c
smart-sachin <sasachin04@gmail.com>
2016-06-26 12:03:01 +0530

    Animtion in main activity

--- app/src/main/java/com/example/megha/movieplate/MainActivity.java
+++ app/src/main/java/com/example/megha/movieplate/MainActivity.java
@@ -3,6 +3,7 @@ package com.example.megha.movieplate;
 import android.content.Context;
 import android.content.Intent;
 import android.content.SharedPreferences;
+import android.os.Handler;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.view.View;
@@ -17,7 +18,7 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
-        iv = (ImageView) findViewById(R.id.imageView);
+
         final SharedPreferences sp = getSharedPreferences("MoviePlate", Context.MODE_PRIVATE);
         boolean api_key_present = sp.getBoolean(Constants.BOOLEAN_API_KEY_PRESENT, false);
         if (! api_key_present){
@@ -25,20 +26,25 @@ public class MainActivity extends AppCompatActivity {
             editor.putString(Constants.API_KEY, "4b21312cf568464ee6b05097ebc6f824");
             editor.commit();
         }
-        iv.setOnClickListener(new View.OnClickListener() {
+
+        new Handler().postDelayed(new Runnable() {
             @Override
-            public void onClick(View v) {
-                Intent i = new Intent();
+            public void run() {
+                Intent intent = new Intent();
                 boolean signedIn = sp.getBoolean(Constants.BOOLEAN_SIGNED_IN, false);
                 if(signedIn){
-                    i.setClass(MainActivity.this, HomeActivity.class);
-                    startActivity(i);
+                    intent.setClass(MainActivity.this, HomeActivity.class);
+
                 }
                 else{
-                    i.setClass(MainActivity.this, SignInScreen.class);
-                    startActivity(i);
+                    intent.setClass(MainActivity.this, SignInScreen.class);
+
                 }
+                startActivity(intent);
+
+                overridePendingTransition(R.animator.fade_in, R.animator.fade_out);
+                finish();
             }
-        });
+        }, 2000);
     }
 }
